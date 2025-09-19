cf58975704848dfe799d72d46d49d10c2c6a79e8
Dennis Rua <ruadennis20@gmail.com>
2025-09-19 13:15:18 +0300

    Created Button  component that accepts title and styles arguments

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -11,21 +11,21 @@ const Landing: React.FC = () => {
       <div className="flex gap-4 mt-2">
         <Button title="Small - Rounded Sm" styles="text-sm rounded-sm" />
         <Button title="Small - Rounded Md" styles="text-sm rounded-md" />
-        <Button title="Small - Rounded Full" styles="text-sm rounded-full" />
+        <Button title="Small - Rounded Full" styles="text-sm rounded-lg" />
       </div>
 
       {/* Medium Buttons */}
       <div className="flex gap-4 mt-2">
         <Button title="Medium - Rounded Sm" styles="text-md rounded-sm" />
         <Button title="Medium - Rounded Md" styles="text-md rounded-md" />
-        <Button title="Medium - Rounded Full" styles="text-md rounded-full" />
+        <Button title="Medium - Rounded Full" styles="text-md rounded-lg" />
       </div>
 
       {/* Large Buttons */}
       <div className="flex gap-4 mt-2">
         <Button title="Large - Rounded Sm" styles="text-lg rounded-sm" />
         <Button title="Large - Rounded Md" styles="text-lg rounded-md" />
-        <Button title="Large - Rounded Full" styles="text-lg rounded-full" />
+        <Button title="Large - Rounded Full" styles="text-lg rounded-lg" />
       </div>
     </div>
   );
